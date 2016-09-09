87e129e346c360e9c37b32e0de7851b8ec1cca11
AndreyGorbachov <AndreyGorbatschov@gmail.com>
2016-09-09 17:24:34 +0300

    Add files via upload

---
+++ src/app/Main.java
@@ -0,0 +1,39 @@
+package app;
+
+import java.util.List;
+
+/**
+ * @author Kalitko.Evgeny
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		int maxTreeDepth = getMaxTreeDepth(NodeImpl.testCase01());
+		System.out.println(maxTreeDepth);
+	}
+
+	public static int getMaxTreeDepth(Node arbitraryNode) {
+		while(arbitraryNode.getParent() != null){
+			arbitraryNode = arbitraryNode.getParent();
+		}
+		return getMaxDepth(arbitraryNode);
+	}
+	
+	private static int getMaxDepth(Node node){
+		if (node.getChildren().size() > 0) {
+			List<Node> children = node.getChildren();
+			int maxTreeDepth = 0;
+			int newTreeDepth = 0;
+			for (int i = 0; i < children.size(); i++) {
+				newTreeDepth = getMaxDepth(children.get(i));
+				if (maxTreeDepth < newTreeDepth) {
+					maxTreeDepth = newTreeDepth;
+				}
+			}
+			return maxTreeDepth + 1;
+		}
+		return 0;
+	}
+
+}
